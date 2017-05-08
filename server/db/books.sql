6f5370bf8075ef9a9310835bb0389ab9a084fd3a
Petur Stavrev <stavri94@abv.bg>
2017-05-08 11:37:05 +0300

    kontrolna

---
+++ server/db/books.sql
@@ -0,0 +1,80 @@
+/*
+
+Примерни задачи:
+
+1. Да се създаде базата данни. Да се покажат всички автори в таблицата горе вляво.
+   За целта:
+    - Да се промени рутера на сървъра така че да връща авторите в JSON.
+      Нека държавата на автора да се получава чрез JOIN на countries и authors
+      Примерно URL: (GET) /authors
+    - Промени клиента да визуализира в HTML таблицата с автори.
+
+2. При клик върху името на автор от горната задача да се визуализира таблица с книги долу вляво.
+   Над таблицата да се покаже името на автора и броя на книгите.
+   За целта:
+    - Да се промени рутера на сървъра  така че да връща книги по зададен автор, както и самия автор по зададено ID на автора
+      Примерно URL: (GET) /authors/3/books
+    - Да се промени клиента така че да визуализира таблица с книгите, а над нея името на автора и броя на книгите.
+
+3. Да се поставят линкове за изтриване на автор и книга съответно в горната и долната таблици от задачи 1 и 2.
+   За целта:
+    - Да се промени рутера на сървъра така че да може да изтрива книга по ID и автор по ID.
+      примерно URL: (DELETE) /authors/3   (DELETE) /books/2
+    - Да се промени клиента така че при изобразяване на книги и автори до тях да има линк за изтриване
+      и при натискане на този линк да се изпълнява HTTP DELETE върху URL на книгата или автора.
+
+4. Да се реализира редактиране на автор чрез HTML форма (тага <form>) и проверки за валидност на сървъра.
+   За целта:
+   - Да се имплементира в рутера на сървъра четене и запис на автор по зададено ID.
+     Примерно URL: (GET) /authors/3 (POST) /authors/3
+   - Да се имплементира в клиента рендериране на HTML форма с подходящи контроли за редактиране на полетата на автора,
+     както и логика, която да изпраща данните към сървъра.
+*/
+
+CREATE TABLE `authors` (
+  `id` int(11) NOT NULL,
+  `A_name` varchar(96) NOT NULL,
+  `age` int(11) NOT NULL,
+  `country_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `authors` (`id`, `A_name`, `age`, `country_id`) VALUES
+(1, 'Arthur Clarke', 90, 1),
+(2, 'J. R. R. Tolkien', 120, 2),
+(3, 'Иван Вазов', 100, 359);
+
+CREATE TABLE `books` (
+  `id` int(11) NOT NULL,
+  `title` varchar(128) NOT NULL,
+  `pages` int(11) NOT NULL,
+  `year` int(11) NOT NULL,
+  `author_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `books` (`id`, `title`, `pages`, `year`, `author_id`) VALUES
+(1, 'Lord of the Rings', 1200, 1961, 2),
+(2, 'Childhood\'s End', 200, 1971, 1),
+(3, 'Под Игото', 900, 1892, 3);
+
+CREATE TABLE `countries` (
+  `id` int(11) NOT NULL,
+  `name` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `countries` (`id`, `name`) VALUES
+(1, 'US'),
+(2, 'England'),
+(359, 'България');
+
+ALTER TABLE `authors`
+  ADD PRIMARY KEY (`id`);
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`id`);
+ALTER TABLE `countries`
+  ADD PRIMARY KEY (`id`);
+ALTER TABLE `authors`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `books`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `countries`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=360;
---
+++ server/router.php
@@ -0,0 +1,122 @@
+<?php
+
+// Route URL paths
+if($request->get('authors')) {
+	$response->authors = $db->querybind_all('SELECT authors.id, A_name, age, name FROM authors INNER JOIN countries ON authors.country_id=countries.id');
+
+}
+// else if($request->get('authors/[0-9]+')) {
+// 	$authorID = (int) $request->segment(1);
+// 	$response->person = $db->querybind_one('SELECT * FROM authors WHERE id = ?', [ $authorID ]);
+// 	if(!$response->person) {
+// 		$response->code(404);
+// 		$response->error('404: Person Not Found.');
+// 	}
+// }
+// else if($request->post('authors/[0-9]+') || $request->post('persons')) {
+// 	$person_id = (int) $request->segment(1, 0);
+// 	$person = $request->data;
+// 	if($person) {
+// 		if(strlen($person->fname) < 1) $response->error('First Name is empty.');
+// 		if(strlen($person->lname) < 1) $response->error('Last Name is empty.');
+// 		if(strlen($person->address) < 3) $response->error('Address is shorter then 3 characters.');
+// 	}
+// 	else {
+// 		$response->error('No JSON data sent.');
+// 	}
+//
+// 	if($response->hasErrors()) {
+// 		$response->code(400);
+// 		$response->error('400: Invalid input.');
+// 	}
+// 	else {
+// 		if($person_id > 0) { // update existing
+// 			$result = $db->querybind(
+// 				'UPDATE persons SET fname=?, lname=?, address=? WHERE id=?',
+// 				[$person->fname, $person->lname, $person->address, $person_id]
+// 			);
+// 		} else { // insert new
+// 			$result = $db->querybind(
+// 				'INSERT INTO persons SET fname=?, lname=?, address=?',
+// 				[$person->fname, $person->lname, $person->address]
+// 			);
+// 			$person_id = $db->insert_id;
+// 		}
+//
+// 		$response->person = $db->querybind_one('SELECT * FROM persons WHERE id = ?', [$person_id]);
+// 		$response->info('Person saved.');
+// 	}
+// }
+// else if($request->delete('persons/[0-9]+')) {
+// 	$person_id = (int) $request->segment(1);
+// 	$db->querybind('DELETE FROM telephones WHERE person_id = ?', [$person_id] );
+// 	$db->querybind('DELETE FROM persons WHERE id = ?', [$person_id] );
+// 	$response->info("Person id=$person_id and its telephones deleted.");
+// }
+// else if($request->get('persons/[0-9]+/telephones')) {
+// 	$person_id = (int) $request->segment(1);
+// 	$response->person = $db->querybind_one('SELECT * FROM persons WHERE id = ?', [$person_id] );
+// 	$response->telephones = [];
+// 	if($response->person) {
+// 		$response->telephones = $db->querybind_all('SELECT * FROM telephones WHERE person_id = ?', [$person_id] );
+// 	}
+// 	else {
+// 		$response->code(404);
+// 		$response->error("404: Person id=$person_id not found.");
+// 	}
+// }
+// else if($request->get('telephones/[0-9]+')) {
+// 	$telephone_id = (int) $request->segment(1);
+// 	$response->telephone = $db->querybind_one('SELECT * FROM telephones WHERE id = ?', [ $telephone_id ]);
+// 	if(!$response->telephone) {
+// 		$response->code(404);
+// 		$response->error('404: Telephone Not Found.');
+// 	}
+// }
+// else if($request->post('telephones/[0-9]+') || $request->post('telephones')) {
+// 	$telephone_id = (int) $request->segment(1);
+// 	$telephone = $request->data; // deserialized JSON object sent over the network.
+// 	if($telephone) {
+// 		if(strlen($telephone->number) < 1) $response->error('Number is empty.');
+// 		if($telephone->person_id < 1) $response->error('Missing person_id.');
+// 		if($telephone->teltype_id < 1) $response->error('Type is empty.');
+// //		$teltype = $db->querybind_one("SELECT * FROM teltypes WHERE id = ?", [$telephone->teltype_id + 0]);
+// //		if(!$teltype) $response->error('Type is invalid.');
+// 	}
+// 	else {
+// 		$response->error('No JSON data sent.');
+// 	}
+//
+// 	if($response->hasErrors()) {
+// 		$response->code(400);
+// 		$response->error('400: Invalid input.');
+// 	}
+// 	else {
+// 		$args = [$telephone->person_id, $telephone->teltype_id, $telephone->number, $telephone_id];
+//
+// 		if($telephone_id > 0) { // update existing
+// 			$result = $db->querybind('UPDATE telephones SET person_id=?, teltype_id=?, number=? WHERE id=?', $args);
+// 		} else { // insert new
+// 			$result = $db->querybind('INSERT INTO telephones SET person_id=?, teltype_id=?, number=?', $args);
+// 			$telephone_id = $db->insert_id;
+// 		}
+//
+// 		$response->telephone = $db->querybind_one('SELECT * FROM telephones WHERE id = ?', [$telephone_id]);
+// 		$response->info('Telephone saved.');
+// 	}
+// }
+// else if($request->delete('telephones/[0-9]+')) {
+// 	$telephone_id = (int) $request->segment(1);
+// 	$db->querybind('DELETE FROM telephones WHERE id = ?', [$telephone_id] );
+// 	$response->info("Telephone id=$telephone_id deleted.");
+// }
+// else if($request->get('teltypes')) {
+// 	$response->teltypes = $db->querybind_all('SELECT * FROM teltypes ORDER BY id');
+// }
+else {
+	$response->error('404: URL Not Found: /'.$request->path);
+	$response->code(404);
+}
+
+// Outputs $response object as JSON to the client.
+echo $response->render();
